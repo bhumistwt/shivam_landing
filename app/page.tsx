40fcfb26e12f51cea5e7feac724102a5f3be3ec8
bhumistwt <bhumimishra99600@gmail.com>
2025-02-11 23:33:30 +0530

    shivam landing

--- app/page.tsx
+++ app/page.tsx
@@ -2,30 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+      <h2 className="text-lg text-black font-semibold mt-10">NEW GREEN DEAL CORPORATION'S</h2>
+      <h1 className="text-6xl text-black font-bold mt-2">WEB3 SLIDE DECK</h1>
+       
+        <ol className=" text-black text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
+          Welcome to the Future of Sustainable Blockchain where services are
+          decentralized, secure, privacy ensured, and eco-friendly.
           </li>
-          <li>Save and see your changes instantly.</li>
+          
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            className="rounded-full text-white bg-black border bg-b border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
@@ -37,15 +30,15 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            www.ncog.earth →
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            className="rounded-full text-black border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#181616] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            info@ncog.earth →
           </a>
         </div>
       </main>
@@ -63,7 +56,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -78,7 +71,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -93,9 +86,10 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+         
         </a>
       </footer>
     </div>
+    
   );
 }
